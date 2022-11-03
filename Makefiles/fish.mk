d6b7a1c7c1519a27356028f6079878f77fb2f366
s0ran <s0ran.n.11921@gmail.com>
2022-11-03 15:42:07 +0900

    linuxのエラーの原因調査

--- Makefiles/fish.mk
+++ Makefiles/fish.mk
@@ -31,22 +31,13 @@ brew-packages: brew ~/.Brewfile
 # Dependencies for Windows
 sudo:
 	curl -s https://raw.githubusercontent.com/imachug/win-sudo/master/install.sh | sh
-# Chocolatey Installation for Windows
-choco:
-	@echo "Installing choco"
-	powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
-check-choco: choco
-	@echo "Checking choco"
-	@echo $(PATH)
-	@echo $(VPATH)
-	@$(CHOCOLATEY_ROOT)/bin/choco -v
-	@choco -v
 
 # SETUP FISH
 ifeq ($(MODE), "minimum")
 fish: $(FISH_DEPENDENCIES)
 	@$(INSTALL_FISH)
+	@echo $(INSTALL_FISH)
 else
 fish: brew-packages
 endif
@@ -54,12 +45,14 @@ endif
 	@mkdir -p ~/.config/fish
 
 ~/.config/fish/config.fish: |~/.config/fish
+	@echo "Installing fish config"
 	@cp config/fish/config.fish ~/.config/fish/config.fish
 
 ~/.config/fish/fish_plugins: |~/.config/fish
 	@cp config/fish/fish_plugins ~/.config/fish/fish_plugins
 
 chsh-fish:fish ~/.config/fish/config.fish
+	@echo $(FISH_PATH)
 	$(eval FISH_PATH := $(shell which fish))
 	@echo $(FISH_PATH)
 ifeq ($(shell cat /etc/shells | grep fish),)
