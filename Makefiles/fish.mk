d925f966bbefd6bd0985853c85cd054797ed274b
s0ran <s0ran.n.11921@gmail.com>
2022-11-04 13:48:56 +0900

    戻す

--- Makefiles/fish.mk
+++ Makefiles/fish.mk
@@ -33,7 +33,7 @@ ifeq ($(shell cat /etc/shells | grep fish),)
 	@echo `which fish` | sudo tee -a /etc/shells
 endif
 	@sudo chsh -s `which fish`
-$(eval SHELL := $(variable which fish))
+	$(eval SHELL := $(variable which fish))
 check-fish: ~/.config/fish/config.fish chsh-fish
 	@echo "Checking fish"
 	@fish -v
