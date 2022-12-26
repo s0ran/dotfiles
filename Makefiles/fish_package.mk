0e5b02bd48b300c0e8dcdf05d0060ad37969dc9e
s0ran <s0ran.n.11921@gmail.com>
2022-12-26 17:21:06 +0900

    デバッグ

--- Makefiles/fish_package.mk
+++ Makefiles/fish_package.mk
@@ -5,7 +5,8 @@ all: fish-packages
 ~/.config/fish/fish_plugins:
 	@cp config/fish/fish_plugins ~/.config/fish/fish_plugins
 fisher:
-	@curl -sL https://git.io/fisher |source && sleep 3 && fisher install jorgebucaran/fisher
+	which fish
+	@curl -sL https://git.io/fisher | source && sleep 3 && fisher install jorgebucaran/fisher
 	@fisher -v
 fish-packages: fisher ~/.config/fish/fish_plugins
 	@fisher update
