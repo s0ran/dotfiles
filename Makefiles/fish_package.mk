b460320361b6c5419acc571f70dcd40603af0f0c
s0ran <s0ran.n.11921@gmail.com>
2022-12-26 17:55:31 +0900

    修正デバッグ

--- Makefiles/fish_package.mk
+++ Makefiles/fish_package.mk
@@ -4,11 +4,11 @@ MAKESHELL := $(shell which make)
 
 all: fish-packages
 ~/.config/fish/fish_plugins:
+	@cp config/fish/fish_plugins ~/.config/fish/fish_plugins
+fisher:
 	@echo COMSPEC: $$COMSPEC
 	@echo SHELL: $$SHELL
 	@echo MAKESHELL: $$MAKESHELL
-	@cp config/fish/fish_plugins ~/.config/fish/fish_plugins
-fisher:
 	@curl -sL https://git.io/fisher | source && sleep 3 && fisher install jorgebucaran/fisher
 	@fisher -v
 fish-packages: fisher ~/.config/fish/fish_plugins
