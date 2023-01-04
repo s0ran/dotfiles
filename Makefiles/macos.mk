87e6fbc1e0b928e0e36a9eea54c577ac2612d753
s0ran <s0ran.n.11921@gmail.com>
2023-01-05 00:07:45 +0900

    /usr/local/brew対応

--- Makefiles/macos.mk
+++ Makefiles/macos.mk
@@ -1,7 +1,12 @@
-HOMEBREW_PREFIX := /opt/homebrew
+BREW_PATH := $(shell which brew)
+ifeq ($(BREW_PATH),)
+HOMEBREW_PREFIX:=/opt/homebrew
 export PATH:= $(HOMEBREW_PREFIX)/bin:$(HOMEBREW_PREFIX)/sbin:$(shell echo "$$PATH")
 PACKAGE_ROOT:=$(HOMEBREW_PREFIX)/bin
 BREW_PATH:=$(PACKAGE_ROOT)/brew
+else
+PACKAGE_ROOT:=$(shell dirname BREW_PATH)
+endif
 FISH_DEPENDENCIES := $(BREW_PATH)
 BREW_INSTALL := brew install
 INSTALL_FISH := $(BREW_INSTALL) fish
