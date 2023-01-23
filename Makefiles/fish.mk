b5556d4ab1cf0b859ef452f5f61ab4209525d6fb
s0ran <s0ran.n.11921@gmail.com>
2023-01-23 11:09:20 +0900

    fix: fish

--- Makefiles/fish.mk
+++ Makefiles/fish.mk
@@ -1,10 +1,11 @@
+.PHONY: all
 # fishがインストールされていれば、fishのシェルが登録されているかチェックする
 # なければ、fishをインストールする
 # そして、fishのシェルを登録する
 export
 FISH_CONFIG_DIR:=${HOME}/.config/fish
 
-all: fish/chsh ${EMPTY_TARGET}/fish/chsh
+all: ${EMPTY_TARGET}/fish/chsh
 	@$(MAKE) -e --makefile=Makefiles/fish_packages.mk all
 
 $(FISH_PATH) ${EMPTY_TARGET}/fish &:
@@ -12,15 +13,16 @@ $(FISH_PATH) ${EMPTY_TARGET}/fish &:
 	@mkdir -p ${EMPTY_TARGET}/fish
 
 ${FISH_CONFIG_DIR}:
-	ln -s ${PWD}/config/fish ${FISH_CONFIG_DIR}
+	@ln -s ${PWD}/config/fish $@
 
 #${FISH_CONFIG_DIR}/config.fish: ${FISH_CONFIG_DIR} config/fish/config.fish
 #	@cp config/fish/config.fish ${FISH_CONFIG_DIR}/config.fish
 
-fish/chsh ${EMPTY_TARGET}/fish/chsh &: $(FISH_PATH) ${EMPTY_TARGET}/fish ${FISH_CONFIG_DIR}
+${EMPTY_TARGET}/fish/chsh: $(FISH_PATH) ${EMPTY_TARGET}/fish ${FISH_CONFIG_DIR}
 ifeq ($(shell cat /etc/shells | grep fish),)
 	@echo `which fish` | sudo tee -a /etc/shells
 endif
+	echo ${FISH_CONFIG_DIR}
 	@sudo chsh -s `which fish`
 	@touch ${EMPTY_TARGET}/fish/chsh
 
