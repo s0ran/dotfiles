582c12dcb064d2f5a464e3d51bb7ad51583ad33e
s0ran <s0ran.n.11921@gmail.com>
2023-02-03 00:20:35 +0900

    chore(macos): add comment

--- Makefiles/macos.mk
+++ Makefiles/macos.mk
@@ -1,15 +1,21 @@
 BREW_PATH := $(shell which brew)
+
+# brew is not installed
 ifeq ($(BREW_PATH),)
 HOMEBREW_PREFIX:=/opt/homebrew
 export PATH:= $(HOMEBREW_PREFIX)/bin:$(HOMEBREW_PREFIX)/sbin:$(shell echo "$$PATH")
 PACKAGE_ROOT:=$(HOMEBREW_PREFIX)/bin
 BREW_PATH:=$(PACKAGE_ROOT)/brew
 else
-PACKAGE_ROOT:=$(shell dirname BREW_PATH)
+PACKAGE_ROOT:=$(shell dirname ${BREW_PATH})
 endif
+
+# set for fish install
 FISH_DEPENDENCIES := $(BREW_PATH)
 BREW_INSTALL := brew install
 INSTALL_FISH := $(BREW_INSTALL) fish
+
+# fish is not installed
 ifeq ($(FISH_PATH),)
 FISH_PATH := $(PACKAGE_ROOT)/fish
 endif
