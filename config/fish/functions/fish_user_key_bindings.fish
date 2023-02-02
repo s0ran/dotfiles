bf7399dc5891e00300b1cdee388e31c8434fe1ce
s0ran <s0ran.n.11921@gmail.com>
2023-02-03 01:08:31 +0900

    feat: use emacs bind in Insert mode

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
-	fish_vi_key_bindings
+	fish_default_key_bindings -M insert
+	fish_vi_key_bindings --no-erase
 	bind \cr peco_select_history
 	bind -M default \ecopy copy_line
 end
