5b9d408e0994a2961b21542ef5b23d350fb4af66
s0ran <s0ran.n.11921@gmail.com>
2023-02-18 16:18:58 +0900

    refactor: refactor

--- Makefiles/fish_packages.mk
+++ Makefiles/fish_packages.mk
@@ -7,14 +7,12 @@ FISH_FUNCTIONS_DIR := ${HOME}/.config/fish/functions
 
 all: fish/packages
 
-${FISH_CONFIG_DIR}: ${FISH_FUNCTIONS_DIR}/fisher.fish
-# rm -rf ${FISH_CONFIG_DIR}
-	ln -sf ${PWD}/config/fish ${FISH_CONFIG_DIR}
-	ln -sf ${PWD}/config/fish/fish_plugins ${FISH_CONFIG_DIR}/fish_plugins
-
-
 ${FISH_FUNCTIONS_DIR}/fisher.fish:
 	@curl -sL https://git.io/fisher | source && sleep 3 && fisher install jorgebucaran/fisher
+
+${FISH_CONFIG_DIR}: ${FISH_FUNCTIONS_DIR}/fisher.fish
+	ln -sf ${PWD}/config/fish ${FISH_CONFIG_DIR}
+	ln -sf ${PWD}/config/fish/fish_plugins ${FISH_CONFIG_DIR}/fish_plugins
 
 fish/packages: ${FISH_CONFIG_DIR}
 	cat ~/.config/fish/fish_plugins
