353ead173f66eaaf6153a4b7f306033abafe03c8
s0ran <s0ran.n.11921@gmail.com>
2023-02-18 15:47:50 +0900

    fix: remove verbose link

--- Makefiles/fish_packages.mk
+++ Makefiles/fish_packages.mk
@@ -9,17 +9,13 @@ all: fish/packages
 
 ${FISH_CONFIG_DIR}: ${FISH_FUNCTIONS_DIR}/fisher.fish
 	@ln -sf ${PWD}/config/fish $@
-	cat ~/.config/fish/fish_plugins
-	cat config/fish/fish_plugins
 
-~/.config/fish/fish_plugins: ${FISH_CONFIG_DIR}
-	@ln -sf ${PWD}/config/fish/fish_plugins ~/.config/fish/fish_plugins
-	cat ~/.config/fish/fish_plugins
 
 ${FISH_FUNCTIONS_DIR}/fisher.fish:
 	@curl -sL https://git.io/fisher | source && sleep 3 && fisher install jorgebucaran/fisher
 
-fish/packages: ~/.config/fish/fish_plugins
+fish/packages: ${FISH_CONFIG_DIR}
+	cat ~/.config/fish/fish_plugins
 	ifeq ($(UNAME),$(filter $(UNAME),Darwin Linux))
 		@fisher update
 	else
