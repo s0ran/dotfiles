ed2b8a04e42ea288f805c7e6e5672e5a7bd64b89
s0ran <s0ran.n.11921@gmail.com>
2023-02-18 15:42:04 +0900

    fix: verbose link

--- Makefiles/fish_packages.mk
+++ Makefiles/fish_packages.mk
@@ -12,12 +12,12 @@ ${FISH_CONFIG_DIR}: ${FISH_FUNCTIONS_DIR}/fisher.fish
 	cat ~/.config/fish/fish_plugins
 	cat config/fish/fish_plugins
 
-#~/.config/fish/fish_plugins: ${FISH_FUNCTIONS_DIR}/fisher.fish
-#	@ln -sf ${PWD}/config/fish/fish_plugins ~/.config/fish/fish_plugins
+~/.config/fish/fish_plugins: ${FISH_FUNCTIONS_DIR}
+	@ln -sf ${PWD}/config/fish/fish_plugins ~/.config/fish/fish_plugins
 
 ${FISH_FUNCTIONS_DIR}/fisher.fish:
 	@curl -sL https://git.io/fisher | source && sleep 3 && fisher install jorgebucaran/fisher
 
-fish/packages: ${FISH_CONFIG_DIR}
+fish/packages: ~/.config/fish/fish_plugins
 	@fisher update
 	@touch ${EMPTY_TARGET}/fish/packages
