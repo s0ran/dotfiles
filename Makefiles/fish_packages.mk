3704d65e913fa9fae3da26842479fa5752f22e79
s0ran <s0ran.n.11921@gmail.com>
2023-02-18 15:22:36 +0900

    fix: rearchitect dependency

--- Makefiles/fish_packages.mk
+++ Makefiles/fish_packages.mk
@@ -7,12 +7,12 @@ FISH_FUNCTIONS_DIR := ${HOME}/.config/fish/functions
 
 all: fish/packages
 
-~/.config/fish/fish_plugins: config/fish/fish_plugins
+~/.config/fish/fish_plugins: ${FISH_FUNCTIONS_DIR}/fisher.fish
 	@ln -sf ${PWD}/config/fish/fish_plugins ~/.config/fish/fish_plugins
 
 ${FISH_FUNCTIONS_DIR}/fisher.fish:
 	@curl -sL https://git.io/fisher | source && sleep 3 && fisher install jorgebucaran/fisher
 
-fish/packages: ${FISH_FUNCTIONS_DIR}/fisher.fish ~/.config/fish/fish_plugins
+fish/packages: ~/.config/fish/fish_plugins
 	@fish -c fisher update
 	@touch ${EMPTY_TARGET}/fish/packages
